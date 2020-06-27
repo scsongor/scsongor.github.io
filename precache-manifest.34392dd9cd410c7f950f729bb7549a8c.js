e211213e5c5870ceaa76f953e0cd3647e670aeeb
Csongor Sovany <sovanycsongor@gmail.com>
2020-06-27 13:05:35 +0100

    Updates

---
+++ precache-manifest.34392dd9cd410c7f950f729bb7549a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02bccd40f252791c499a9d6dce4e58f",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "afea0de456ef9099f5c0",
+    "url": "/app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fab3c1cf399127029d0f",
+    "url": "/app/static/js/2.6cb6d0fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/static/js/2.6cb6d0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afea0de456ef9099f5c0",
+    "url": "/app/static/js/main.e148ff77.chunk.js"
+  },
+  {
+    "revision": "fb683d993c694aeb9312",
+    "url": "/app/static/js/runtime-main.95af9303.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  }
+]);
